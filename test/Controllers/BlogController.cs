8fe679fe846180d9ae247e7f7e08a6d52c10db6c
nisann97 <nisann@code.edu.az>
2024-05-12 08:12:30 +0400

    added

---
+++ test/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using test.Data;
+using test.ViewModels;
+using test.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Blogs.Where(m => !m.SoftDeleted).ToListAsync());
+        }
+        
+    }
+}
+
---
+++ test/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using test.Data;
+using test.Models;
+using test.ViewModels;
+
+namespace test.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IActionResult>Index()
+    {
+        List<Slider> sliders = await _context.Sliders.ToListAsync();
+        SliderInfo sliderInfo = await _context.SlidersInfo.FirstOrDefaultAsync();
+        List<Category> categories = await _context.Categories.Where(m => !m.SoftDeleted).ToListAsync();
+        List<Product> products = await _context.Products.Include(m => m.ProductImages).ToListAsync();
+        Surprise surprise = await _context.Surprises.FirstOrDefaultAsync();
+        List<SurpriseBulletPoints> surpriseBulletPoints = await  _context.SurpriseBulletPoints.ToListAsync();
+        ExpertPanel expertPanel = await _context.ExpertPanel.FirstOrDefaultAsync();
+        List<Expert> experts = await _context.Experts.ToListAsync();
+        List<Blog> blogs = await _context.Blogs.Where(m=>!m.SoftDeleted).Take(3).ToListAsync();
+
+        HomeVM model = new()
+        {
+            Sliders = sliders,
+            SliderInfo = sliderInfo,
+            Categories = categories,
+            Products = products,
+            Surprise = surprise,
+            SurpriseBulletPoints = surpriseBulletPoints,
+            ExpertPanel = expertPanel,
+            Experts = experts,
+            Blogs = blogs
+
+        };
+
+        return View(model);
+    }
+
+}
+
+
+
